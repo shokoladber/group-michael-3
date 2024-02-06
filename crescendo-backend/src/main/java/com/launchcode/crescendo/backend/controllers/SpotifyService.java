6200f738ab95ca694cab745486c59368179b8292
Sarah Dao <sarahdao@Sarahs-MBP-3.attlocal.net>
2024-02-06 10:04:58 -0600

    added models and classes for the spotify API

---
+++ crescendo-backend/src/main/java/com/launchcode/crescendo/backend/controllers/SpotifyService.java
@@ -0,0 +1,2 @@
+package com.launchcode.crescendo.backend.controllers;public class SpotifyService {
+}
---
+++ crescendo-backend/src/main/java/com/launchcode/crescendo/backend/models/SpotifyTracks.java
@@ -0,0 +1,2 @@
+package com.launchcode.crescendo.backend.models;public class SpotifyTracks {
+}
